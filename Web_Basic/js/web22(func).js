dfee8827262aa05e0d769f17dc2fe9d2c85e5580
SAVAGE6974 <jaeminan944@icloud.com>
2025-06-09 14:31:33 +0900

    add html ad js

---
+++ Web_Basic/js/web22(func).js
@@ -0,0 +1,46 @@
+// function addNumber(){
+//     let num1 = 2;
+//     let num2 = 3;
+//     let sum = num1 + num2;
+//     console.log(`결과값: ${sum}`);
+// }
+
+// addNumber();
+// addNumber();
+
+// var sum = 0;
+// function addNumber() {
+//     // var result;
+//     sum = 10 + 20;
+//     result = 10 * 20;
+// }
+
+// addNumber();
+// console.log(sum);
+// console.log(result);
+
+// var x = 10;
+// function displayNumber() {
+//     console.log(`x is ${x}`);
+//     console.log(`y is ${y}`);
+//     let y = 20;
+// }
+
+// displayNumber();
+
+// var a = 2;
+// var a = 5;
+// console.log(a);
+
+// let b = 2;
+// b = 5;
+// console.log(b);
+
+function multiple(a, b = 5, c = 10) {
+    return a * b + c;
+}
+
+console.log(multiple(5, 10, 20));
+console.log(multiple(10, 20));
+console.log(multiple(30));
+
