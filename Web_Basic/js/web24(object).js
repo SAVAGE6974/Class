a9f1d5becff92d34f59d80cfaae9e1571601e05e
SAVAGE6974 <jaeminan944@icloud.com>
2025-06-12 11:09:35 +0900

    Update web24(object).js

--- Web_Basic/js/web24(object).js
+++ Web_Basic/js/web24(object).js
@@ -1,4 +1,6 @@
-let numvers = ["one", "two", "three", "four"];
-for(let i = 0; i < numvers.length; i++) {
-    console.log(numvers[i]);
-}
+let date_1 = new Date("2024");
+let date_2 = new Date("2024-02");
+let date_3 = new Date("2024-02-03");
+let date_4 = new Date("2024-02-03T18:00:00");
+
+console.log(date_4);
